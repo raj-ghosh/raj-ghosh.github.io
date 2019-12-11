9747419d5c32fa17e5e182020d7da15762354e2f
Raj Ghosh <i.a.m.r.a.j.gh@gmail.com>
2019-12-11 09:28:55 +0530

    added files

---
+++ scripts/main.js
@@ -0,0 +1,175 @@
+window.onload = function(){
+   this.setTimeout(function(){
+   var loader = this.document.querySelector(".loader-container");
+   var bars = this.document.querySelector(".bar");
+    loader.style.backgroundColor = "transparent";
+    bars.style.backgroundColor = "rgba(0,0,0,0)";
+        setTimeout(function(){
+            loader.style.display = "none";
+        }, 500);
+   }, 3000);
+}
+
+var hamburger = document.querySelector(".hamburger-menu");
+var b1 = document.querySelector(".one");
+var b2 = document.querySelector(".two");
+var b3 = document.querySelector(".three");
+var panel = document.querySelector(".left-panel");
+var cline = document.querySelector(".c-line");
+var pline = document.querySelector(".p-line");
+var port = document.querySelector(".port");
+var cont = document.querySelector(".cont");
+var p_info = document.querySelector(".p-info");
+var is_clicked = true;
+
+function hamOpen() {
+    panel.style.left = "-285px";
+    b1.style.width = "15px";
+    b2.style.width = "15px";
+    b2.style.transform = "translateY(8px)"
+    b3.style.width = "30px";
+    b3.style.transform = "translateY(-8px)"
+    p_info.style.left = "-285px";
+    p_info.style.width = "1162px";
+}
+
+function hamClose() {
+    panel.style.left = "0";
+    b1.style.width = "30px";
+    b2.style.width = "15px";
+    b2.style.transform = "translateY(0px)";
+    b3.style.width = "30px";
+    b3.style.transform = "translateY(0px)";
+    p_info.style.left = "0px";
+    p_info.style.width = "877px";
+}
+
+function ChoverIn() {
+    cline.style.transform = "scaleX(4)";
+}
+
+function ChoverOut() {
+    cline.style.transform = "scaleX(1)";
+}
+
+
+function PhoverIn() {
+    pline.style.transform = "scaleX(5)";
+}
+
+function PhoverOut() {
+    pline.style.transform = "scaleX(1)";
+}
+
+setTimeout(() => {
+    hamburger.click();
+}, 0);
+
+hamburger.onclick = function() {
+    if(is_clicked){
+        is_clicked = false;
+        hamClose();
+    } else {
+        is_clicked = true;
+        hamOpen();
+    }
+}
+
+cont.onmouseover = function() {
+    ChoverIn();
+}
+port.onmouseover = function() {
+    PhoverIn();
+}
+cont.onmouseout = function() {
+    ChoverOut();
+}
+port.onmouseout = function() {
+    PhoverOut();
+}
+
+// P-info 
+
+var addr = document.querySelector(".addr");
+var eml = document.querySelector(".eml");
+var mob = document.querySelector(".mob");
+var s_addr = document.querySelector(".s-addr");
+var s_eml = document.querySelector(".s-eml");
+var s_mob = document.querySelector(".s-mob");
+var pointerI = document.querySelector(".fa-map-marker-alt");
+var mailI = document.querySelector(".fa-envelope");
+var callI = document.querySelector(".fa-phone-alt");
+
+addr.onmouseover = function() {
+    addr.style.backgroundColor = "#2c2c2c";
+    addr.style.boxShadow = "0px 0px 10px 0px #191919";
+    s_addr.style.backgroundColor = "#2c2c2c";
+    s_addr.style.boxShadow = "0px 0px 10px 0px #191919";
+    pointerI.style.transform = "translateX(130px)";
+    pointerI.style.color = "#f73838";
+}
+addr.onmouseout = function() {
+    addr.style.backgroundColor = "";
+    addr.style.boxShadow = "";
+    s_addr.style.backgroundColor = "";
+    s_addr.style.boxShadow = "";
+    pointerI.style.transform = "translateX(90px)";
+    pointerI.style.color = "white";
+}
+
+eml.onmouseover = function() {
+    eml.style.backgroundColor = "#2c2c2c";
+    eml.style.boxShadow = "0px 0px 10px 0px #191919";
+    s_eml.style.backgroundColor = "#2c2c2c";
+    s_eml.style.boxShadow = "0px 0px 10px 0px #191919";
+    mailI.style.transform = "translateX(130px)";
+    mailI.style.color = "dodgerblue";
+}
+
+eml.onmouseout = function() {
+    eml.style.backgroundColor = "";
+    eml.style.boxShadow = "";
+    s_eml.style.backgroundColor = "";
+    s_eml.style.boxShadow = "";
+    mailI.style.transform = "translateX(90px)";
+    mailI.style.color = "white";
+}
+
+mob.onmouseover = function() {
+    mob.style.backgroundColor = "#2c2c2c";
+    mob.style.boxShadow = "0px 0px 10px 0px #191919";
+    s_mob.style.backgroundColor = "#2c2c2c";
+    s_mob.style.boxShadow = "0px 0px 10px 0px #191919";
+    callI.style.transform = "translateX(130px)";
+    callI.style.color = "#21ff3b";
+}
+
+mob.onmouseout = function() {
+    mob.style.backgroundColor = "";
+    mob.style.boxShadow = "";
+    s_mob.style.backgroundColor = "";
+    s_mob.style.boxShadow = "";
+    callI.style.transform = "translateX(90px)";
+    callI.style.color = "white";
+}
+
+
+// Contact Form Modal
+
+var contactModal = document.querySelector(".contact-modal");
+
+function openContact() {
+    setTimeout(function(){
+        contactModal.style.opacity = "100%";
+    },250);
+    setTimeout(function(){
+        contactModal.style.display = "block";
+    }, 0);
+}
+
+function closeContact() {
+    contactModal.style.opacity = "0%";
+    setTimeout(function(){
+        contactModal.style.display = "none";
+    }, 250);
+}
